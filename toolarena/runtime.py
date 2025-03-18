bde67b83017ab32a03f4204337f9c8c34d93e5b1
Georg Wölflein <georgw7777@gmail.com>
2025-03-18 11:15:20 +0100

    Docker

--- toolarena/runtime.py
+++ toolarena/runtime.py
@@ -1,4 +1,4 @@
-"""This is the client that runs inside the Docker container."""
+"""This is the client that communicates with the tool runtime running inside a Docker container."""
 
 import os
 import shutil
@@ -7,21 +7,22 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Self, Sequence
 
-import docker  # type: ignore
+import docker
 import tenacity
-from docker.models.containers import Container  # type: ignore
-from docker.models.images import Image  # type: ignore
-from docker.types import DeviceRequest, Mount  # type: ignore
+from docker.errors import NotFound as DockerNotFoundError
+from docker.models.containers import Container
+from docker.models.images import Image
+from docker.types import DeviceRequest, Mount
 from httpx import AsyncClient, HTTPError
 from loguru import logger
 
 from toolarena.types import ArgumentType, RunToolResponse
-from toolarena.utils import ROOT_DIR
+from toolarena.utils import ROOT_DIR, join_paths, rmdir
 
 type MountMapping = Mapping[str, str]  # host -> container
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, kw_only=True)
 class HTTPToolClient:
     host: str
     port: int
@@ -60,237 +61,204 @@ class HTTPToolClient:
             )
 
 
-# DAVINCI_RUNTIME_IMAGE = "davinci-runtime"
-# RUNTIME_BUILD_CONTEXT = ROOT_DIR
-# RUNTIME_DOCKERFILE = RUNTIME_BUILD_CONTEXT / "docker" / "runtime.Dockerfile"
-# INSTALLED_DOCKERFILE = RUNTIME_BUILD_CONTEXT / "docker" / "installed.Dockerfile"
+RUNTIME_IMAGE = "toolarena-runtime"
+RUNTIME_BUILD_CONTEXT = ROOT_DIR
+RUNTIME_DOCKERFILE = RUNTIME_BUILD_CONTEXT / "docker" / "runtime.Dockerfile"
+TOOL_DOCKERFILE = RUNTIME_BUILD_CONTEXT / "docker" / "tool.Dockerfile"
 
 
-# def get_docker() -> docker.DockerClient:
-#     return docker.from_env(timeout=480)  # increase timeout to avoid timeout errors
+def get_docker() -> docker.DockerClient:
+    return docker.from_env(timeout=480)  # increase timeout to avoid timeout errors
 
 
-# @dataclass(frozen=True)
-# class Mounts:
-#     input: Path | None = None  # folder to mount as input
-#     output: Path | None = None  # folder to mount as output
-#     data_dir: Path | None = BENCHMARK_DIR / "data"  # folder to copy input data from
-#     input_mapping: MountMapping | None = None  # mapping of input data to mount
+@dataclass(frozen=True)
+class Mounts:
+    input: Path | None = None  # folder to mount as input
+    output: Path | None = None  # folder to mount as output
+    data_dir: Path | None = None  # folder to copy input data from
+    input_mapping: MountMapping | None = None  # mapping of input data to mount
 
-#     def to_docker(self) -> Sequence[Mount]:
-#         mounts = []
-#         if self.input is not None:
-#             mounts.append(
-#                 Mount(
-#                     target="/mount/input",
-#                     source=str(Path(self.input).resolve()),
-#                     type="bind",
-#                     read_only=False,  # TODO: make read-only?
-#                 )
-#             )
-#         if self.output is not None:
-#             mounts.append(
-#                 Mount(
-#                     target="/mount/output",
-#                     source=str(Path(self.output).resolve()),
-#                     type="bind",
-#                     read_only=False,
-#                 )
-#             )
-#         return mounts
+    def to_docker(self) -> list[Mount]:
+        mounts = []
+        if self.input is not None:
+            mounts.append(
+                Mount(
+                    target="/mount/input",
+                    source=str(Path(self.input).resolve()),
+                    type="bind",
+                    read_only=False,  # TODO: make read-only?
+                )
+            )
+        if self.output is not None:
+            mounts.append(
+                Mount(
+                    target="/mount/output",
+                    source=str(Path(self.output).resolve()),
+                    type="bind",
+                    read_only=False,
+                )
+            )
+        return mounts
 
-#     def reset(self) -> None:
-#         """Setup the input and output mounts by copying data."""
-#         if self.output:
-#             rmdir(self.output)
-#             self.output.mkdir(parents=True, exist_ok=True)
+    def reset(self) -> None:
+        """Setup the input and output mounts by copying data."""
+        if self.output:
+            rmdir(self.output)
+            self.output.mkdir(parents=True, exist_ok=True)
 
-#         if self.input:
-#             rmdir(self.input)
-#             self.input.mkdir(parents=True, exist_ok=True)
-#             if not self.input_mapping:
-#                 logger.debug("Not creating any input mounts...")
-#                 return
-#             if not self.data_dir:
-#                 raise ValueError("data_dir is required")
+        if self.input:
+            rmdir(self.input)
+            self.input.mkdir(parents=True, exist_ok=True)
+            if not self.input_mapping:
+                logger.debug("Not creating any input mounts...")
+                return
+            if not self.data_dir:
+                raise ValueError("data_dir is required")
 
-#             for src, dst in self.input_mapping.items():
-#                 src_path = join_paths(self.data_dir, src)
-#                 dst_path = join_paths(self.input, dst)
-#                 logger.debug(f"Copying {src_path} to {dst_path}")
-#                 if not src_path.exists():
-#                     raise FileNotFoundError(f"Input data not found: {src_path}")
-#                 dst_path.parent.mkdir(parents=True, exist_ok=True)
-#                 if src_path.is_dir():
-#                     shutil.copytree(src_path, dst_path)
-#                 else:
-#                     shutil.copy(src_path, dst_path)
-
-
-# def build_image(
-#     repository: str = DAVINCI_RUNTIME_IMAGE,
-#     *,
-#     tag: str,
-#     context: Path | str = RUNTIME_BUILD_CONTEXT,
-#     dockerfile: Path | str = RUNTIME_DOCKERFILE,
-# ) -> Image:
-#     """Build the image from docker/runtime.Dockerfile using BuildKit."""
-#     logger.debug(f"Building image {repository}:{tag} using Docker BuildKit")
-#     image, output = get_docker().images.build(
-#         path=str(context),
-#         dockerfile=str(dockerfile),
-#         tag=f"{repository}:{tag}",
-#         buildargs={"DOCKER_BUILDKIT": "1"},
-#     )
-#     for record in output:
-#         try:
-#             print(record["stream"], end="")
-#         except Exception:
-#             logger.debug(f"Error printing build output: {record}")
-#             break
-#     logger.info(f"Built image {repository}:{tag} using Docker BuildKit")
-#     return image
+            for src, dst in self.input_mapping.items():
+                src_path = join_paths(self.data_dir, src)
+                dst_path = join_paths(self.input, dst)
+                logger.debug(f"Copying {src_path} to {dst_path}")
+                if not src_path.exists():
+                    raise FileNotFoundError(f"Input data not found: {src_path}")
+                dst_path.parent.mkdir(parents=True, exist_ok=True)
+                if src_path.is_dir():
+                    shutil.copytree(src_path, dst_path)
+                else:
+                    shutil.copy(src_path, dst_path)
 
 
-# @dataclass(frozen=True)
-# class DockerRuntimeClient(HTTPRuntimeClient):
-#     name: str  # the name of the container
-#     repository: str  # the docker repository of the image (i.e. name of the image)
-#     tag: str  # the docker tag of the image
+def build_image(
+    repository: str = RUNTIME_IMAGE,
+    *,
+    tag: str,
+    context: Path | str = RUNTIME_BUILD_CONTEXT,
+    dockerfile: Path | str = RUNTIME_DOCKERFILE,
+) -> Image:
+    """Build the image from docker/runtime.Dockerfile using BuildKit."""
+    logger.debug(f"Building image {repository}:{tag} using Docker BuildKit")
+    image, output = get_docker().images.build(
+        path=str(context),
+        dockerfile=str(dockerfile),
+        tag=f"{repository}:{tag}",
+        buildargs={"DOCKER_BUILDKIT": "1"},
+    )
+    for record in output:
+        try:
+            print(record["stream"], end="")  # type: ignore
+        except Exception:
+            logger.debug(f"Error printing build output: {record}")
+            break
+    logger.info(f"Built image {repository}:{tag} using Docker BuildKit")
+    return image
 
-#     @classmethod
-#     def _start_container(
-#         cls,
-#         image: str,
-#         name: str,
-#         port: int,
-#         tag: str = "latest",
-#         mounts: Mounts | None = None,
-#         gpus: Sequence[str] | None = None,
-#         env: Mapping[str, str] = {},
-#     ) -> Container:
-#         device_requests = []
-#         if gpus is None:
-#             gpus = os.getenv("CUDA_VISIBLE_DEVICES", "").split(",")
-#         if gpus:
-#             device_requests.append(
-#                 DeviceRequest(device_ids=gpus, capabilities=[["gpu"]])
-#             )
-#             if "CUDA_VISIBLE_DEVICES" not in env:
-#                 env = dict(env)
-#                 env["CUDA_VISIBLE_DEVICES"] = ",".join(str(i) for i in range(len(gpus)))
-#         logger.debug(f"Starting container with GPUs {gpus} on port {port}")
 
-#         # Start a container with the supplied name
-#         container = get_docker().containers.run(
-#             image=f"{image}:{tag}",
-#             name=name,
-#             detach=True,
-#             ports={8000: port},
-#             tty=True,
-#             mounts=(mounts or Mounts()).to_docker(),
-#             mem_limit="100g",
-#             shm_size="10g",
-#             device_requests=device_requests,
-#             environment=env,
-#         )
-#         logger.info(
-#             f"Started runtime client {container.name} on port {port} from image {image}:{tag}"
-#         )
-#         return container
+@dataclass(frozen=True, kw_only=True)
+class DockerRuntimeClient(HTTPToolClient):
+    name: str  # the name of the container
+    repository: str  # the docker repository of the image (i.e. name of the image)
+    tag: str  # the docker tag of the image
 
-#     @classmethod
-#     def from_container(cls, container: Container, port: int | None = None) -> Self:
-#         repository, tag = container.image.tags[0].split(":")
-#         return cls(
-#             host="localhost",
-#             port=port or int(container.ports["8000/tcp"][0]["HostPort"]),
-#             name=container.name,
-#             repository=repository,
-#             tag=tag,
-#         )
+    @classmethod
+    def _start_container(
+        cls,
+        image: str,
+        name: str,
+        port: int,
+        tag: str = "latest",
+        mounts: Mounts | None = None,
+        gpus: Sequence[str] | None = None,
+        env: Mapping[str, str] = {},
+    ) -> Container:
+        device_requests = []
+        if gpus is None:
+            gpus = os.getenv("CUDA_VISIBLE_DEVICES", "").split(",")
+        if gpus:
+            device_requests.append(
+                DeviceRequest(device_ids=gpus, capabilities=[["gpu"]])
+            )
+            if "CUDA_VISIBLE_DEVICES" not in env:
+                env = dict(env)
+                env["CUDA_VISIBLE_DEVICES"] = ",".join(str(i) for i in range(len(gpus)))
+        logger.debug(f"Starting container with GPUs {gpus} on port {port}")
 
-#     @classmethod
-#     def create(
-#         cls,
-#         name: str,
-#         port: int = int(os.getenv("DAVINCI_RUNTIME_PORT", "8000")),
-#         reuse_existing: bool = True,
-#         build: bool = False,
-#         image: str = DAVINCI_RUNTIME_IMAGE,
-#         tag: str = "latest",
-#         timeout: float | None = 10.0,  # max wait time for the runtime to become ready
-#         mounts: Mounts | None = None,
-#         gpus: Sequence[str] | None = None,
-#         env: Mapping[str, str] = {},
-#         allow_build: bool = True,
-#     ) -> Self:
-#         """Create a new runtime client by building the image and starting the container."""
-#         client = get_docker()
+        # Start a container with the supplied name
+        container = get_docker().containers.run(
+            image=f"{image}:{tag}",
+            name=name,
+            detach=True,
+            ports={"8000/tcp": port},
+            tty=True,
+            mounts=(mounts or Mounts()).to_docker(),
+            mem_limit="100g",
+            shm_size="10g",
+            device_requests=device_requests,
+            environment=dict(env),
+        )
+        logger.info(
+            f"Started runtime client {container.name} on port {port} from image {image}:{tag}"
+        )
+        return container
 
-#         try:
-#             container = client.containers.get(name)
-#             logger.info(f"Found existing container {name}")
-#             if reuse_existing and mounts is None:
-#                 for tag in container.image.tags:
-#                     container_image, container_tag = tag.split(":")
-#                     if container_image == image and container_tag == tag:
-#                         return cls.from_container(container)
-#             container.remove(force=True)
-#             logger.info(f"Removed existing container {name}")
-#         except docker.errors.NotFound:
-#             pass
+    @classmethod
+    def from_container(cls, container: Container, port: int | None = None) -> Self:
+        repository, tag = container.image.tags[0].split(":")  # type: ignore[union-attr]
+        return cls(
+            host="localhost",
+            port=port or int(container.ports["8000/tcp"][0]["HostPort"]),
+            name=container.name,  # type: ignore
+            repository=repository,
+            tag=tag,
+        )
 
-#         if build or not client.images.list(f"{image}:{tag}"):
-#             if not allow_build:
-#                 raise RuntimeError(
-#                     f"Image {image}:{tag} not found and build is not allowed"
-#                 )
-#             build_image(image, tag=tag)
+    @classmethod
+    def create(
+        cls,
+        name: str,
+        port: int = int(os.getenv("TOOLARENA_RUNTIME_PORT", "8000")),
+        reuse_existing: bool = True,
+        build: bool = False,
+        image: str = RUNTIME_IMAGE,
+        tag: str = "latest",
+        timeout: float | None = 10.0,  # max wait time for the runtime to become ready
+        mounts: Mounts | None = None,
+        gpus: Sequence[str] | None = None,
+        env: Mapping[str, str] = {},
+        allow_build: bool = True,
+    ) -> Self:
+        """Create a new runtime client by building the image and starting the container."""
+        client = get_docker()
 
-#         client = cls.from_container(
-#             cls._start_container(
-#                 image, name, port=port, tag=tag, mounts=mounts, gpus=gpus, env=env
-#             ),
-#             port=port,
-#         )
-#         client.wait_for_alive(timeout=timeout)
-#         return client
+        try:
+            container = client.containers.get(name)
+            logger.info(f"Found existing container {name}")
+            if reuse_existing and mounts is None:
+                for tag in container.image.tags:  # type: ignore[union-attr]
+                    container_image, container_tag = tag.split(":")
+                    if container_image == image and container_tag == tag:
+                        return cls.from_container(container)
+            container.remove(force=True)
+            logger.info(f"Removed existing container {name}")
+        except DockerNotFoundError:
+            pass
 
-#     def stop(self):
-#         container = get_docker().containers.get(self.name)
-#         container.stop()
-#         container.remove(force=True)
+        if build or not client.images.list(f"{image}:{tag}"):
+            if not allow_build:
+                raise RuntimeError(
+                    f"Image {image}:{tag} not found and build is not allowed"
+                )
+            build_image(image, tag=tag)
 
-#     def save_checkpoint(self, tag: str) -> None:
-#         logger.info(
-#             f"Saving checkpoint {self.repository}:{tag} for container {self.name}"
-#         )
-#         container = get_docker().containers.get(self.name)
-#         container.commit(repository=self.repository, tag=tag)
+        client = cls.from_container(
+            cls._start_container(
+                image, name, port=port, tag=tag, mounts=mounts, gpus=gpus, env=env
+            ),
+            port=port,
+        )
+        client.wait_for_alive(timeout=timeout)
+        return client
 
-#     @classmethod
-#     def load_checkpoint(
-#         cls,
-#         name: str,
-#         tag: str,
-#         image: str = DAVINCI_RUNTIME_IMAGE,
-#         reuse_existing: bool = False,
-#         mounts: Mounts | None = None,
-#         gpus: Sequence[str] | None = None,
-#         env: Mapping[str, str] = {},
-#         build: bool = False,  # force build
-#         allow_build: bool = True,
-#     ) -> Self:
-#         logger.info(f"Loading checkpoint {image}:{tag} as {name}")
-#         return cls.create(
-#             name,
-#             reuse_existing=reuse_existing,
-#             build=build,
-#             allow_build=allow_build,
-#             image=image,
-#             tag=tag,
-#             mounts=mounts,
-#             gpus=gpus,
-#             env=env,
-#         )
+    def stop(self):
+        container = get_docker().containers.get(self.name)
+        container.stop()
+        container.remove(force=True)
--- toolarena/utils.py
+++ toolarena/utils.py
@@ -1,3 +1,48 @@
+import os
+import shutil
 from pathlib import Path
 
+from loguru import logger
+
 ROOT_DIR = Path(__file__).parent.parent
+
+
+def join_paths(parent: os.PathLike | str, *children: os.PathLike | str) -> Path:
+    """Join paths and ensure that the resulting path is relative to the parent."""
+    path = Path(parent)
+    for child in children:
+        path = path / Path(child)
+    if not path.resolve().is_relative_to(Path(parent).resolve()):
+        raise ValueError(f"Path {path} is not relative to {parent}")
+
+    return path
+
+
+def chown_dir_using_docker(
+    dir: os.PathLike | str, uid: int = os.getuid(), gid: int = os.getgid()
+) -> None:
+    import docker  # type: ignore
+
+    client = docker.from_env()
+    logger.debug(f"Chowning directory {dir} to {uid}:{gid} using docker")
+    client.containers.run(
+        "busybox",
+        ["chown", "-R", f"{uid}:{gid}", "/mnt/mydir"],
+        volumes={str(Path(dir).resolve()): {"bind": "/mnt/mydir", "mode": "rw"}},
+        auto_remove=True,
+    )
+
+
+def rmdir(dir: os.PathLike | str) -> None:
+    try:
+        if Path(dir).exists():
+            shutil.rmtree(dir)
+    except PermissionError:
+        logger.warning(
+            f"Failed to remove directory {dir}. "
+            "This may happen because docker wrote files with permissions that are not writable by the current user. "
+            "We will try to chown the directory to the current user."
+        )
+        chown_dir_using_docker(dir)
+        shutil.rmtree(dir)
+    logger.debug(f"Removed directory {dir}.")
