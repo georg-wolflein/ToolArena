e07023c4fe58b365c49644e0df43e09674e680c5
Georg Wölflein <georgw7777@gmail.com>
2025-04-07 16:13:50 +0200

    Rename

--- push_docker.sh
+++ push_docker.sh
@@ -1,3 +1,3 @@
 docker build -t toolarena-runtime -f docker/runtime.Dockerfile .
-docker tag toolarena-runtime ghcr.io/georg-wolflein/toolarena-runtime:latest
-docker push ghcr.io/georg-wolflein/toolarena-runtime:latest
+docker tag toolarena-runtime ghcr.io/georg-wolflein/ToolArena:latest
+docker push ghcr.io/georg-wolflein/ToolArena:latest
