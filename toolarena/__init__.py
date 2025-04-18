b37c71887a30fbb1ac74e6e9f103e69fc7767035
Georg Wölflein <georgw7777@gmail.com>
2025-04-18 16:35:04 +0200

    Ensure environment variables are loaded from .env file

--- toolarena/__init__.py
+++ toolarena/__init__.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+
 from toolarena.definition import Repository, ToolDefinition, ToolInvocation
 from toolarena.run import ToolImplementation, ToolRunner, ToolRunResult
 from toolarena.utils import RUNS_DIR, TASKS_DIR
+
+load_dotenv()
