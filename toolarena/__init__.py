1af57c46591e38cdc1db820b3e07a9ba1c14684b
Georg Wölflein <georgw7777@gmail.com>
2025-04-26 00:30:27 +0200

    Override environment variables

--- toolarena/__init__.py
+++ toolarena/__init__.py
@@ -4,4 +4,4 @@ from toolarena.definition import Repository, ToolDefinition, ToolInvocation
 from toolarena.run import ToolImplementation, ToolRunner, ToolRunResult
 from toolarena.utils import RUNS_DIR, TASKS_DIR
 
-load_dotenv()
+load_dotenv(override=True)
