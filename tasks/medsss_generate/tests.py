a5ba9f93340710c941c8d38f2ee386c7f59e31bd
Georg Wölflein <georgw7777@gmail.com>
2025-05-04 12:17:50 +0200

    Add medsss_generate task (#18)

---
+++ tasks/medsss_generate/tests.py
@@ -0,0 +1,16 @@
+from tasks.utils import initialize, parametrize_invocation
+from toolarena.run import ToolRunResult
+
+initialize()
+
+
+@parametrize_invocation("nsclc", "motor_vehicle_accident", "pediatric_rash")
+def test_status(invocation: ToolRunResult):
+    assert invocation.status == "success"
+
+
+@parametrize_invocation("nsclc", "motor_vehicle_accident", "pediatric_rash")
+def test_response_is_str(invocation: ToolRunResult):
+    assert "response" in invocation.result
+    response = invocation.result["response"]
+    assert isinstance(response, str)
