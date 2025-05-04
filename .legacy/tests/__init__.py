b8fa3c235fc9b3b6b56f5e98e6bbed7d54015ba2
Georg Wölflein <georgw7777@gmail.com>
2025-05-04 23:40:10 +0200

    Remove .legacy folder

--- .legacy/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Disable DeprecationWarning https://github.com/jupyter/jupyter_core/issues/398
-os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
--- .legacy/tests/data/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains data used by the benchmark tests, e.g. for comparing tool outputs.
