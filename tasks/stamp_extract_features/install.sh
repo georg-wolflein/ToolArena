508b0474b480646883fabecf7100cd76a1e96636
Georg Wölflein <georgw7777@gmail.com>
2025-05-04 22:57:26 +0200

    Remove unnecessary files in stamp_extract_features task

--- tasks/stamp_extract_features/install.sh
+++ tasks/stamp_extract_features/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 git clone https://github.com/KatherLab/STAMP /workspace/STAMP
-cd /workspace/STAMP && git checkout 0042e6f
+cd /workspace/STAMP && git checkout 97522aa
 
-apt-get update && apt-get install -y libgl1-mesa-glx
-pip install -e ".[ctranspath]"
+apt update && apt install -y libgl1 libglx-mesa0 libglib2.0-0
+pip install -e "/workspace/STAMP[ctranspath]"
