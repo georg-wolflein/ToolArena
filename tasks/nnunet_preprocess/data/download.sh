8d9aee5337994ed63f8e6c8d40e9dab65d2f3036
Georg Wölflein <georgw7777@gmail.com>
2025-05-05 19:14:43 +0200

    Prefer downloading from AWS

--- tasks/nnunet_preprocess/data/download.sh
+++ tasks/nnunet_preprocess/data/download.sh
@@ -8,10 +8,15 @@ set -e
 
 mkdir -p msd
 
-uvx gdown "https://drive.google.com/file/d/1wEB2I6S6tQBVEPxir8cA5kFB8gTQadYY/view" --fuzzy -O msd/Task02_Heart.tar
-uvx gdown "https://drive.google.com/file/d/1RzPB1_bqzQhlWvU-YGvZzhx2omcDh38C/view" --fuzzy -O msd/Task04_Hippocampus.tar
-uvx gdown "https://drive.google.com/file/d/1Ff7c21UksxyT4JfETjaarmuKEjdqe1-a/view" --fuzzy -O msd/Task05_Prostate.tar
-uvx gdown "https://drive.google.com/file/d/1jzeNU1EKnK81PyTsrx0ujfNl-t0Jo8uE/view" --fuzzy -O msd/Task09_Spleen.tar
+# uvx gdown "https://drive.google.com/file/d/1wEB2I6S6tQBVEPxir8cA5kFB8gTQadYY/view" --fuzzy -O msd/Task02_Heart.tar
+# uvx gdown "https://drive.google.com/file/d/1RzPB1_bqzQhlWvU-YGvZzhx2omcDh38C/view" --fuzzy -O msd/Task04_Hippocampus.tar
+# uvx gdown "https://drive.google.com/file/d/1Ff7c21UksxyT4JfETjaarmuKEjdqe1-a/view" --fuzzy -O msd/Task05_Prostate.tar
+# uvx gdown "https://drive.google.com/file/d/1jzeNU1EKnK81PyTsrx0ujfNl-t0Jo8uE/view" --fuzzy -O msd/Task09_Spleen.tar
+
+wget -O msd/Task02_Heart.tar "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task02_Heart.tar"
+wget -O msd/Task04_Hippocampus.tar "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task04_Hippocampus.tar"
+wget -O msd/Task05_Prostate.tar "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task05_Prostate.tar"
+wget -O msd/Task09_Spleen.tar "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
 
 tar -xvf msd/Task02_Heart.tar -C msd
 tar -xvf msd/Task04_Hippocampus.tar -C msd
