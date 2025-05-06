22b62822abef7234f79eb2ea7433b6d2406518cd
Georg Wölflein <georgw7777@gmail.com>
2025-05-06 17:29:19 +0200

    Update huggingface dataset from CI

---
+++ scripts/update_huggingface_dataset.py
@@ -0,0 +1,19 @@
+"""Script to update the HuggingFace dataset. Runs on on CI."""
+
+import os
+
+from datasets import Dataset
+from toolarena.definition import ToolDefinition
+from toolarena.utils import TASKS_DIR
+
+HF_REPO_ID = os.getenv("HF_REPO_ID", "KatherLab/ToolArena")
+HF_COMMIT_MESSAGE = os.getenv("HF_COMMIT_MESSAGE", "Update dataset")
+
+if __name__ == "__main__":
+    ds = Dataset.from_list(
+        [
+            ToolDefinition.from_yaml(f).model_dump(mode="json")
+            for f in TASKS_DIR.glob("*/task.yaml")
+        ]
+    )
+    ds.push_to_hub(HF_REPO_ID, commit_message=HF_COMMIT_MESSAGE)
